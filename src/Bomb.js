beee174ebb7f4299889b4c14449593964492028e
Risher Randall <randallr18@mail.wlu.edu>
2018-08-13 18:44:01 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,21 @@
 // your Bomb code here!
+
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  render() {
+    return(
+      <div>
+      {this.state.secondsLeft > 0 ? `${this.state.secondsLeft} seconds left before I go boom!` : "Boom!"}
+      </div>
+    )
+  }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,19 @@
 // your ImageSlider code here!
+
+import React, { Component } from 'react';
+
+export default class ImageSlider extends Component {
+
+  state = {
+    currentSlideIndex: 0
+  }
+
+  render() {
+    return (
+      <div>
+      I am on slide {this.state.currentSlideIndex}
+      </div>
+    )
+  }
+
+}
